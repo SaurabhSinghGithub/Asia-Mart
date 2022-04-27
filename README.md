743b04300d935888d3aa482d92505f4aedc1a306
SaurabhSinghGithub <saurabhsinghgithub@gmail.com>
2022-04-27 16:26:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Asia Mart
+
+Our Motto is “Lets Trade Globally”
+Our product sourcing service researches wholesale suppliers on your behalf for your business needs. Our detailed client focus maintains an effective customer relations and service program serving needs of our suppliers and buyers.
+
---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    let leftNavbar = document.getElementById('left');
+    let rightNavbar = document.getElementById('right');
+    let Nav = document.getElementById('nav');
+
+    leftNavbar.classList.toggle('v-class');
+    rightNavbar.classList.toggle('v-class');
+    Nav.classList.toggle('h-class');
+}
+
